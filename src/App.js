de849ce0a0614dc1fab2ac3fe1a49a8484b12e4d
Kamui Shirou <x_kamui@msn.com>
2022-11-02 05:43:10 +0100

    résolution de la demande

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Word from "./components/word";
 
 function App() {
+  const [words, setWords] = useState("");
+  const fetchAllUrl = "https://baconipsum.com/api/?type=meat-and-filler";
+
+  useEffect(() => {
+    fetch(fetchAllUrl)
+      .then((response) => response.json())
+      .then((data) => setWords(data));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {words?.length > 0
+        ? words[0]
+            .split(" ")
+            .map((word, index) => <Word key={index} word={word} />)
+        : ""}
     </div>
   );
 }
---
+++ src/components/word.js
@@ -0,0 +1,5 @@
+function Word(props) {
+  return <div className="word">{props.word}</div>;
+}
+
+export default Word;
